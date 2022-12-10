53ab71b0652ddc810208a62e79d6b5d27a9d4a1a
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2022-12-10 17:33:21 +0000

    add security trip delete

--- src/main/java/marek/smartluggage/backendsmartluggage/Login/WebSecurityConfig.java
+++ src/main/java/marek/smartluggage/backendsmartluggage/Login/WebSecurityConfig.java
@@ -59,7 +59,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //              .antMatchers("/api/v1/customer").hasAnyRole("USER", "ADMIN")
 //              .antMatchers("/deleteUsers/**").hasAnyRole("USER","ADMIN")
 //               .antMatchers("/api/v1/trip").permitAll()
-              .antMatchers(HttpMethod.OPTIONS,"/api/v1/trip/*").hasRole("ADMIN")
+              .antMatchers(HttpMethod.OPTIONS,"/api/v1/trip/**").hasRole("ADMIN")
+              .antMatchers("/api/v1/trip/**").hasRole("ADMIN")
 
               .antMatchers("/api/v1/customer").permitAll()
               .antMatchers("/deleteUsers/**").permitAll()
