bf08f21585a12ae2194f45db2d7ed9f7f9057d21
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2022-12-10 17:02:32 +0000

    changed permition

--- src/main/java/marek/smartluggage/backendsmartluggage/Login/WebSecurityConfig.java
+++ src/main/java/marek/smartluggage/backendsmartluggage/Login/WebSecurityConfig.java
@@ -73,9 +73,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //              .and().httpBasic();
 //              .and().formLogin();
 
-                .and().formLogin().loginProcessingUrl("https://smartluggagebackend.herokuapp.com/login")
-              .defaultSuccessUrl("https://smartluggage.herokuapp.com/",true).permitAll()
-                .failureForwardUrl("https://frontendsmartluggage.herokuapp.com/")
+//                .and().formLogin().loginProcessingUrl("https://smartluggagebackend.herokuapp.com/login")
+//              .defaultSuccessUrl("https://smartluggage.herokuapp.com/",true).permitAll()
+//                .failureForwardUrl("https://frontendsmartluggage.herokuapp.com/")
               .and().logout().permitAll()
 //              .and().httpBasic()
       ;
